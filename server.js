2378aec6635a1b13aa2fef9742d9bdd376923490
Bao Nguyen <chibaobguyen5585@gmail.com>
2021-09-20 23:36:13 +0700

    Cai dat xu ly loi

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError } = require("./app/helpers/errors");
 
 const app = express();
 
@@ -15,6 +16,17 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
 setupContactRoutes(app);
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 const PORT = config.app.port;
 app.listen(PORT, () => {
